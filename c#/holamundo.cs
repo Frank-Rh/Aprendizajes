09058ce2a9cf6216cb0c0eb80fe4333201354c03
RyogaRH <ryogarh2829@gmail.com>
2021-06-22 02:03:42 -0500

    Create holamundo.cs

---
+++ c#/holamundo.cs
@@ -0,0 +1,12 @@
+//  Este lenguaje es un poco mas complicado (para mi) esto debe que ir dentro de una clase y forzosamente tener un menu
+// aqui no voy a poner los demas recursos por que seria muy pesado, largo y la pc donde lo hago no aguanta visual studio(lo recomiendo para este lenjuage y c++)
+
+using System; // Esto se instala o hace por defecto en el entorno adecuado para trabajar en c#
+
+class Programa // La clase programa
+{   
+    static void Main() // El menu de la clase
+    {
+        Console.WriteLine("Hola Mundo");
+    }
+}
